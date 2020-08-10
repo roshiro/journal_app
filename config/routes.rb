1ea11a8ad4a95011cbd99b1e040cd31a457e697d
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-08-10 21:24:28 +0100

    Improvements to User and creation of Notes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root  to: 'notes#index'
+
+  devise_for :users, path: 'auth'
+
+  resources :users
+  resources :notes
 end
