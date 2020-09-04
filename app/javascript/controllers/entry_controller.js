4aacd416344c919246cd87e338678022d6c5d924
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-04 15:52:05 +0100

    Converting URLs to img tags with Autolinker

--- app/javascript/controllers/entry_controller.js
+++ app/javascript/controllers/entry_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "stimulus"
 import Choices from "choices.js"
+import Autolinker from 'autolinker';
 
 export default class extends Controller {
   connect() {
     this.setupDateComponents()
     this.setupDropdownComponents()
+    this.watchTrixEditor()
   }
 
   setupDateComponents() {
@@ -18,5 +20,43 @@ export default class extends Controller {
 
   setupDropdownComponents() {
     const choices = new Choices('.entry-choices')
+  }
+
+  watchTrixEditor() {
+    var self = this
+
+    addEventListener("trix-change", function(event) {
+      self.convertUrlsToImagesAndUpdateStateUrlCounter()
+    })
+  }
+
+  urlsInText(text) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.match(urlRegex) || []
+  }
+
+  convertUrlsToImagesAndUpdateStateUrlCounter() {
+    var element = document.querySelector("trix-editor")
+    var urlsAddedToText = this.urlsInText(element.value.replaceAll("&nbsp;", " ").replaceAll("\<br\>", " "))
+    if(urlsAddedToText.length > this.data.get('urlCounterInText')) {
+      this.convertImageUrlsToImageTags()
+    }
+    this.data.set('urlCounterInText', urlsAddedToText.length)
+  }
+
+  convertImageUrlsToImageTags() {
+    var linkedText = Autolinker.link(document.querySelector("trix-editor").value, this.autoLinkerOptions())
+    document.querySelector("trix-editor").value = linkedText
+  }
+
+  autoLinkerOptions() {
+    return {
+      replaceFn : function(match) {
+        switch(match.getType()) {
+          case 'url':
+            return `<img src="${match.getAnchorHref()}"/>`
+        }
+      }
+    }
   }
 }
