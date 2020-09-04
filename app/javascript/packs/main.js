1cd20c029c77b9836ebc30446f9da2c7a3df6019
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-04 16:50:51 +0100

    Adding bootstrap

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -3,3 +3,8 @@ require('flatpickr')
 require("flatpickr/dist/themes/dark.css")
 require("choices.js/public/assets/styles/choices.min.css")
 require("vendor")
+
+// Bootstrap and dependencies
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
