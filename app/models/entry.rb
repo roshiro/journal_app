5208748135ef8b5aa11ad0021871455392ca728b
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-10 22:43:05 +0100

    Listing open and closed entries in UI

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :instrument
+  belongs_to :strategy
+  belongs_to :setup
 
   def image_urls_extracted_from_content
     @image_urls ||= CGI.unescapeHTML(content).match(/(https?:\/\/[^\"]+)/).to_a.uniq
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -2,4 +2,5 @@ require "test_helper"
 
 describe Entry do
   should belong_to(:instrument)
+  should belong_to(:strategy)
 end
