ce58afaf382dcd2b834e2fe9935be1071e098bb8
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-10 21:53:22 +0100

    Method to extract links from text

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :instrument
+
+  def image_urls_extracted_from_content
+    @image_urls ||= CGI.unescapeHTML(content).match(/(https?:\/\/[^\"]+)/).to_a.uniq
+  end
 end
