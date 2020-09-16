f0968259de4c9d334c858c493b008d3d284b9044
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-16 17:11:06 +0100

    Improving way to embed tradingview urls

--- app/javascript/controllers/entry_controller.js
+++ app/javascript/controllers/entry_controller.js
@@ -6,7 +6,6 @@ export default class extends Controller {
   connect() {
     this.setupDateComponents()
     this.setupDropdownComponents()
-    this.watchTrixEditor()
   }
 
   setupDateComponents() {
@@ -23,44 +22,5 @@ export default class extends Controller {
       containerOuter: 'choices col-10 p-0 form-control',
       containerInner: 'choices__inner pb-0'
     }})
-  }
-
-  watchTrixEditor() {
-    var self = this
-
-    addEventListener("trix-change", function(event) {
-      console.log('change')
-      self.convertUrlsToImagesAndUpdateStateUrlCounter()
-    })
-  }
-
-  urlsInText(text) {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.match(urlRegex) || []
-  }
-
-  convertUrlsToImagesAndUpdateStateUrlCounter() {
-    var element = document.querySelector("trix-editor")
-    var urlsAddedToText = this.urlsInText(element.value.replaceAll("&nbsp;", " ").replaceAll("\<br\>", " "))
-    if(urlsAddedToText.length > this.data.get('urlCounterInText')) {
-      this.convertImageUrlsToImageTags()
-    }
-    this.data.set('urlCounterInText', urlsAddedToText.length)
-  }
-
-  convertImageUrlsToImageTags() {
-    var linkedText = Autolinker.link(document.querySelector("trix-editor").value, this.autoLinkerOptions())
-    document.querySelector("trix-editor").value = linkedText
-  }
-
-  autoLinkerOptions() {
-    return {
-      replaceFn : function(match) {
-        switch(match.getType()) {
-          case 'url':
-            return `<img src="${match.getAnchorHref()}"/>`
-        }
-      }
-    }
   }
 }
--- app/javascript/vendor/trix-enhancement.js
+++ app/javascript/vendor/trix-enhancement.js
@@ -1,25 +1,45 @@
 var Trix = require("trix");
 
-/* what the newly created button does */
-Trix.config.textAttributes.red = {
-  style: { color: "red" },
-  parser: function(element) {
-    return element.style.color === "red"
+var trixEnhancements = {
+  buttonAttachTradingView: function() {
+    return '<button type="button" id="attach-tradingview-link" class="trix-button icon-attach-tradingview" title="Attach TradingView screenshot" tabindex="-1">Embeed TradingView image</button>';
   },
-  inheritable: true
-}
-/* insert the button visual in the default toolbar */
-addEventListener("trix-initialize", function(event) {
-  var buttonHTML = '<button type="button" data-trix-action="tvlink" class="trix-button icon-attach-tradingview" title="Attach TradingView screenshot" tabindex="-1">Embeed TradingView image</button>'
-      dialog = '<div class="trix-dialog trix-dialog--link trix-active" data-trix-dialog="href" data-trix-dialog-attribute="href" data-trix-active>\
-                  <div class="trix-dialog__link-fields">\
-                    <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="Enter a TradingView URL…" aria-label="URL" required="" data-trix-input="">\
-                    <div class="trix-button-group">\
-                      <input type="button" class="trix-button trix-button--dialog" value="Link" data-trix-method="setAttribute">\
-                    </div>\
-                  </div>\
-                </div>'
 
-  event.target.toolbarElement.querySelector("[data-trix-button-group=file-tools]").insertAdjacentHTML("beforeend", buttonHTML)
-  event.target.toolbarElement.querySelector(".trix-dialogs").insertAdjacentHTML("beforeend", dialog)
-})
+  dialogTradingView: function() {
+    return '<div id="dialog-tv" class="trix-dialog trix-dialog--link trix-active" data-trix-dialog="href" data-trix-dialog-attribute="href" >\
+              <div class="trix-dialog__link-fields">\
+                <input type="url" id="tv-url" name="href" class="trix-input trix-input--dialog" placeholder="Enter a TradingView URL…" aria-label="URL" required="" data-trix-input="">\
+                <div class="trix-button-group">\
+                  <input type="button" id="button-tvlink" class="trix-button trix-button--dialog trix-button-tv" value="Link">\
+                </div>\
+              </div>\
+            </div>';
+  },
+
+  attachEventToLinkButton: function() {
+    document.getElementById('button-tvlink').addEventListener('click', function(event) {
+      var tvUrl = document.getElementById('tv-url').value;
+      var attachment = new Trix.Attachment({ content: `<img src="${tvUrl}" />` })
+      document.querySelector("trix-editor").editor.insertAttachment(attachment)
+      document.getElementById('tv-url').value = ""
+    })
+  },
+
+  attachEventToAttachLinkButon: function() {
+    document.getElementById('attach-tradingview-link').addEventListener('click', function(event) {
+      var dialogElement = document.getElementById('dialog-tv')
+      dialogElement.toggleAttribute('data-trix-active')
+      dialogElement.classList.toggle('trix-active')
+    })
+  }
+};
+
+(function() {
+  /* insert the button visual in the default toolbar */
+  addEventListener("trix-initialize", function(event) {
+    event.target.toolbarElement.querySelector("[data-trix-button-group=file-tools]").insertAdjacentHTML("beforeend", trixEnhancements.buttonAttachTradingView())
+    event.target.toolbarElement.querySelector(".trix-dialogs").insertAdjacentHTML("beforeend", trixEnhancements.dialogTradingView())
+    trixEnhancements.attachEventToLinkButton()
+    trixEnhancements.attachEventToAttachLinkButon()
+  });
+})();
