9310cdeb80ea8dd94b1b9a1f9e7d357461d48552
Rafael Oshiro <rafael.oshiro@gmail.com>
2020-09-16 17:32:18 +0100

    Implementing trade_number

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,15 @@ class Entry < ApplicationRecord
   belongs_to :strategy
   belongs_to :setup
 
+  before_create :increment_trade_number
+
   def image_urls_extracted_from_content
     @image_urls ||= CGI.unescapeHTML(content).match(/(https?:\/\/[^\"]+)/).to_a.uniq
+  end
+
+  private
+
+  def increment_trade_number
+    self.trade_number = (Entry.last&.trade_number || 0) + 1
   end
 end
